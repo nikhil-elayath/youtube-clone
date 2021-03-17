42504a857e8f1e143b07c8173ca7295cca39692f
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-03-17 12:52:00 +0530

    added toggle between buttons in sidebar

---
+++ client/src/components/SearchResultCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SearchResultCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -6,11 +6,40 @@ import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 
 
 export default function SideBar() {
+    const [homeSelected, setHomeSelected]=React.useState(true)
+    const [trendingSelected, setTrendingSelected]=React.useState(false)
+    const [subscriptionSelected, setSubscriptionSelected]=React.useState(false)
+
+
+    const onHomeContainerClick=()=>{
+        console.log("onHomeContainerClick",onHomeContainerClick)
+        setHomeSelected(true)
+        setTrendingSelected(false)
+       setSubscriptionSelected(false)
+       console.log(homeSelected,setTrendingSelected)
+
+    }
+    const onTrendingContainerClick=()=>{
+        console.log("onHomeContainerClick",onHomeContainerClick)
+        setTrendingSelected(true)
+        setHomeSelected(false)
+        setSubscriptionSelected(false)
+
+
+
+    }
+    const onSubsContainerClick=()=>{
+        console.log("onHomeContainerClick",onHomeContainerClick)
+        setSubscriptionSelected(true)
+        setTrendingSelected(false)
+        setHomeSelected(false)
+
+    }
     return (
         <div>
             <div id="mainContainer">
-                <div id="homeContainer">
-                    <div id="homeIcon">
+                <div id= {homeSelected==true? "selectedHomeContainer": "homeContainer"} onClick={onHomeContainerClick}>
+                    <div id={homeSelected==true?"selectedHomeIcon":"homeIcon"}>
                     <HomeIcon/>                        
                          </div>
                     <div>
@@ -18,8 +47,8 @@ export default function SideBar() {
                         </div>
             
                     </div>
-                <div id="trendingContainer">
-                <div id="trendingIcon">
+                <div id= {trendingSelected==true? "selectedTrendingContainer": "trendingContainer"} onClick={onTrendingContainerClick}>
+                <div id={trendingSelected==true?"selectedTrendingIcon":"trendingIcon"}>
                     <WhatshotIcon/>                        
                          </div>
                     <div>
@@ -27,8 +56,8 @@ export default function SideBar() {
                         </div>
             
                     </div>
-                <div id="subscriptionContainer">
-                <div id="subIcon">
+                <div id= {subscriptionSelected==true? "selectedSubscriptionContainer": "subscriptionContainer"} onClick={onSubsContainerClick}>
+                <div id={subscriptionSelected==true?"selectedSubscriptionIcon":"subscriptionIcon"}>
                     <SubscriptionsIcon/>                        
                          </div>
                     <div>
