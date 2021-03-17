c1b8d74b82a9ce61a57621c16267ee0d6de49743
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-03-17 11:58:14 +0530

    added text and icons fro top three

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,92 +1,43 @@
 import React from 'react'
 import "../assests/styles/SideBar.css";
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 
 
 export default function SideBar() {
     return (
         <div>
             <div id="mainContainer">
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
-            this is the sidebar
+                <div id="homeContainer">
+                    <div>
+                    <HomeIcon/>                        
+                         </div>
+                    <div>
+                        Home
+                        </div>
+            
+                    </div>
+                <div id="trendingContainer">
+                    <div>
+                    <WhatshotIcon/>                        
+                         </div>
+                    <div>
+                        Trending
+                        </div>
+            
+                    </div>
+                <div id="subscriptionContainer">
+                    <div>
+                    <SubscriptionsIcon/>                        
+                         </div>
+                    <div>
+                        Subscriptions
+                        </div>
+            
+                    </div>
+          
+         
             </div>
         </div>
     )
